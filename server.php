c5aadaa6cdfd7ebf55fa7a83d0c29abb68271b37
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-03-17 01:07:51 -0500

    implementacion de alertas

--- server.php
+++ server.php
@@ -1,3 +1,31 @@
 <?php
+// funcion para validar que el formulario no llego vacio al servidor
+function validateData($name, $email, $subject, $message, $btnSend)
+{
+    return !empty(trim($name)) && !empty(trim($email)) && !empty(trim($subject)) && !empty(trim($message));
+}
+
+if (isset($_POST["btnSend"])) {
+    if (validateData(...$_POST)) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
 
 
+        $status = "success";
+        echo '<form action="index.php" method="post" id="status-form">';
+        echo '<input type="hidden" name="status" value="' . $status . '">';
+        echo '</form>';
+        echo '<script>document.getElementById("status-form").submit();</script>';
+    } else {
+
+        $status = "danger";
+        echo '<form action="index.php" method="post" id="status-form">';
+        echo '<input type="hidden" name="status" value="' . $status . '">';
+        echo '</form>';
+        echo '<script>document.getElementById("status-form").submit();</script>';
+    }
+} else {
+    echo "el servidor no ha recibido ninguna peticion";
+}
